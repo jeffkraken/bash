40bfb3f1dcf7aa5bbe07ab2ea180dfe9937b4b73
abandonfreewifi <153031981+jeffkraken@users.noreply.github.com>
2025-01-21 15:06:15 -0600

    Update fwrules.sh

--- fwrules.sh
+++ fwrules.sh
@@ -2,6 +2,9 @@
 #This script is to be used for configuring AdGuard Home on Rocky Linux as a part of a guided demo.
 #The configs in this script are base/generic, do not use on a live machine.
 #If used, be sure to go back and update the rules to block unnecessary traffic.
+#Download to VM with the following commmand:
+#curl -sSL https://raw.githubusercontent.com/jeffkraken/bash/refs/heads/main/fwrules.sh
+#After downloading, edit the permissions so that your user can run the script... then run the script.
 
 ipadd=$(ip -4 a | grep 'dynamic' | cut -d ' ' -f6)
 echo "Detected IP: $ipadd"
