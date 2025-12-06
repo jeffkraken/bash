a449a2f18209b7645bf4e85ff2280994fb12cf43
abandonfreewifi <153031981+jeffkraken@users.noreply.github.com>
2025-12-06 12:55:23 -0600

    Update ssh2bandit.sh
    I totally forgot that this script existed. I should clean up my github. It's messy

--- ssh2bandit.sh
+++ ssh2bandit.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-
+# updated 12/6/2025 based on bodydoubling video
 # Used to connect to the OTW wargame "Bandit"
 
 # Define the hostname and port
 HOST="bandit.labs.overthewire.org"
-PORT=2220
 read -p "Enter the username..." USER
 
-# Challenge for students
-# Can you add something that allows you to pass the password as a part of this script?
-
 # Connect to Bandit
-ssh -p $PORT $USER@$HOST
+ssh -p 2220 $USER@$HOST
+
+# For automating, install sshpass (ex. sudo apt install sshpass). 
+# Then comment out the ssh line above.
+# Then uncomment the following.
+#PSWD=$(cat pswd | grep $NAME | awk '{print $3}' | head -n 1)
+#sshpass -p $PSWD ssh $USER@$HOUST -p 2220
